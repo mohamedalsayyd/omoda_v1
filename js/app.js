4f86719be95ba33fe527b08a47b767e99e7bbfbc
Lime <alsayyd.medo72@gmail.com>
2023-08-17 10:33:37 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+import translation from "./translation.js";
+const languageSelector = document.getElementById("slct");
+languageSelector.addEventListener("change", (event) => {
+  localStorage.setItem("lang", event.target.value);
+  setLanguage(event.target.value);
+});
+const languageSelectorMob = document.getElementById("moblieSelect");
+languageSelectorMob.addEventListener("change", (event) => {
+  localStorage.setItem("lang", event.target.value);
+  setLanguage(event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en";
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  languageSelector.value = language;
+  languageSelectorMob.value = language;
+  const elements = document.querySelectorAll("[data-trans]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-trans");
+    element.textContent = translation[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+  const lang = localStorage.getItem("lang") || "en";
+  const textCentered = document.querySelectorAll(".feature-content__title");
+  const textDesc = document.querySelectorAll(".feature-content__text");
+  if (lang === "ar") {
+    textCentered.forEach((item) => {
+      item.classList.add("text-arabic");
+    });
+    textDesc.forEach((item) => {
+      item.classList.add("text-arabic");
+    });
+  } else {
+    textCentered.forEach((item) => {
+      item.classList.remove("text-arabic");
+    });
+    textDesc.forEach((item) => {
+      item.classList.remove("text-arabic");
+    });
+  }
+};
---
+++ js/translation.js
@@ -0,0 +1,160 @@
+const translation = {
+  en: {
+    omoda: "Omoda",
+    brand: "Brand",
+    contact: "Contact",
+    home: "Home",
+    omodac5: "Omoda C5",
+    future: "THE FUTURE STARTS WITH OMODA",
+    futureText:
+      "The future was developed within the framework of the Art in Motion concept. This concept is based on avant-garde art and serves as an experimental platform, exploring new ideas regarding concept, color, and form. The design philosophy of Art in Motion revolves around the belief that functional design can be aesthetically pleasing, evoking a tactile sensation, captivating to the eye, and providing an immersive experience for the driver.",
+    about: "About model",
+    power: "Power, hp",
+    torque: "Torque, Nm",
+    acc: "Acceleration to 100 km/h, sec",
+    fuel: "Fuel consumption, l/100km",
+    art: "ART IN MOTION",
+    artDesc:
+      "The sporty modern look of the vehicle is accentuated by various features, including a stylish grille, 18 two-tone alloy wheels, a floating roof, dynamic fastback bodywork, T-shaped daytime running lights, and dynamic turn signals. Each element is carefully designed to emphasize the overall sporty aesthetic of the vehicle.",
+    led: "Advanced LED optics",
+    ledDesc:
+      "T-shaped daytime running lights ensure enhanced visibility. At night, the powerful LED headlights offer improved illumination, ensuring optimal visibility. Additionally, the matrix taillights feature an original design that creates a captivating 3D effect, further enhancing the futuristic appearance of the car. These elements collectively contribute to the striking and futuristic aesthetic of the vehicle. ",
+    futuristic: "Futuristic silhouette",
+    futuristicDesc:
+      "Swift, sculpted silhouette emphasizes sportiness, reduces aerodynamic drag and reduces fuel consumption",
+    roof: "Floating roof",
+    roofDesc:
+      "Adding visual brilliance to the car is the floating roof, an effect created by the black finish of the B-pillars and C-pillars and the tinted rear windows.",
+    comfort: "PREMIUM COMFORT",
+    comfortDesc:
+      "The front seats are made according to the canons of sports cars - a high one-piece headrest, advanced lateral support for seat cushions, perforated high-quality eco-leather.",
+    seats: "Sports seats",
+    seatsDesc:
+      "The sporty front seats are heated and ventilated and feature a variety of electrical adjustments for driver and passenger comfort.",
+    safety: "DYNAMICS AND SAFETY",
+    safetyDesc:
+      "The synergy of the efficient 1.5Turbo engine and CVT9 ensures excellent driving dynamics and comfort. The engine is paired with a reliable and durable gearbox, equipped with a Honeywell turbocharger and DVVT technology.",
+    performance: "Performance and environmental friendliness",
+    performanceDesc:
+      "The maximum power of the 1.5Turbo engine is 147 hp, themaximum torque is 210 Nm / 1750-4500 rpm.",
+    ai: "Intelligent ADAS systems",
+    aiDesc:
+      "ADAS includes: adaptive cruise control, forward collision warning, autonomous emergency braking, lane departure warning, lane keeping and automatic low/high beam switching.",
+    smart: "SMART TECHNOLOGIES",
+    smartDesc:
+      "The tech interior of the new OMODA has many advanced features: 2 high-resolution dual screens of 10.25” each, multi-color atmospheric interior lighting, wireless charging, high-quality acoustics and much more.",
+    digital: "Digital instrument panel",
+    digitalDesc:
+      "The instrument panel and multimedia complex are presented in the form of dual LCD displays with a diagonal of 10.25 inches with HD resolution and support for Android Auto and Apple CarPlay.",
+    charge: "Wireless phone charging",
+    chargeDesc:
+      "The wireless charging module for 2 smartphones not only efficiently charges, but also informs you if the device is not installed in the charging slot.",
+    remote: "Remote engine start",
+    remoteDesc:
+      "Remote start technology allows you to start the engine with a smart key, and comfort access allows you to open the car when approaching..",
+    sec: "OMODA shares the same mind with every OMODER, plays together, and is the most futuristic crossover SUV brand that gets the most out of young people.",
+    universe: "O‑UNIVERSE",
+    universeDesc:
+      "OMODA is a key that can access parallel universes, through which you can meet yourself in another space and explore infinite possibilities. OMODA respects every kind of lifestyle and inspires every OMODER to be who they are, showing infinite brilliance through OMODA. Come through the future, OMODA, CROSS FROM FUTURE, CROSS FOR FUTURE.",
+    community: "COMMUNITY OF LIKE-MINDED PEOPLE",
+    communityDesc:
+      "They are united by incredible energy, the desire to create, continuously improve and generate new ideas. Which of them will come true, we will find out very soon - after all, the future is much closer than it seems.",
+    club: "O‑CLUB",
+    clubDesc:
+      "Without fear of taking risks, with extraordinary thinking, being creative and willing to accept new things...O-CLUB is the exclusive fan community of OMODERs, we will build a global virtual social platform, where we can play together with global users, share the same heart, and keep creating good memories...",
+    sec2: "At the forefront of OMODA are our ambassadors, whose achievements and records are on the verge of human capabilities, beyond the reach.",
+    life: "O‑LIFE",
+    lifeDesc:
+      "O-LIFE advocates the concept of green low-carbon, environmental protection and energy saving brought by high technology, representing the future life scenes of intelligent ecology + intelligent life.",
+    lab: "O‑LAB",
+    labDesc:
+      "O-LAB is a future-oriented research platform as well as a laboratory for co-creation of creativity and inspiration. We keep developing, researching and creating new technologies, sharing and experimenting with every user; innovating and practicing with every user, and exploring every uncharted territory together.",
+    fashion: "O-FASHION",
+    fashionDesc1:
+      "O-People try their hand at art, music, fashion, destroying internal barriers and expanding their horizons. An unlimited pass to an unforgettable experience that opens up to OMODA owners and friends of the brand. Join the O-People lifestyle and become part of unique events and activities that the brand's team has prepared especially for you.",
+    fashionDesc2:
+      " is a technology-driven aesthetic concept, integrating minimalism and futurism, representing the aesthetic trend of the future crowd. It also embraces a highly intelligent wearable fashion combo with an advanced design concept that presents a futuristic aesthetic style",
+    understand: "Understand more",
+    contactus: "Contact us",
+  },
+  ar: {
+    omoda: "أُمودا",
+    brand: "الهوية",
+    contact: "تواصل معنا ",
+    home: "الرئيسية",
+    omodac5: "أُمودا C5",
+    future: "المستقبل يبدأ مع أُمودا ",
+    futureText:
+      "تم تطويرها ضمن إطار فكرة فن الحركة، والتي تعتمد على الفن التجريبي، كتجربة - مع المفهوم، اللون، والشكل. تعتمد فلسفة تصميم فن الحركة على فكرة أن التصميم الوظيفي يمكن أن يكون أيضًا جميلًا للغاية، قابل للمس، ومرئي من الخارج ويمكن تجربته وراء عجلة القيادة.",
+    about: "عن النموذج",
+    power: "القوة، حصان ",
+    torque: "عزم الدوران ,Nm",
+    acc: "التسارع 9,9  ,  إلى 100 كم/ساعة، ثانية ",
+    fuel: " 7,1 استهلاك الوقود  ,  ل/100 كم  ",
+    art: "الفن في الحركة",
+    artDesc:
+      "شبكة أنيقة، جنوط و سبائك مزدوجة بحجم 18 بوصة، سقف عائم، هيكل خلفي ديناميكي سريع، أضواء نهارية على شكل T، إشارات إلكترونية ديناميكية - كل عنصر يؤكد الطابع الرياضي الحديث.",
+    led: "تقنية الأضواء الثنائية المتقدمة",
+    ledDesc:
+      "تجذب أضواء التشغيل النهاري بشكل حرف T الإنتباه في حركة المرور، وتوفر المصابيح الأمامية المزودة بتقنية LED القوية رؤية ليلية جيدة، وتصميم المصابيح الخلفية الثلاثية الأبعاد الأصلية يخلق تأثير ثلاثي الأبعاد - لمسة ساطعة أخرى في مظهر سيارة المستقبل.",
+    futuristic: "سقف عائم",
+    futuristicDesc:
+      "إضافة لمعان بصري للسيارة بشكل السقف العائم، وهو تأثير يتم إنشاؤه بواسطة اللون الأسود للأعمدة و البي والسي والنوافذ الخلفية الملونة.",
+    roof: "سقف عائم",
+    roofDesc:
+      "إضافة لمعان بصري للسيارة بشكل السقف العائم، وهو تأثير يتم إنشاؤه بواسطة اللون الأسود للأعمدة و البي والسي والنوافذ الخلفية الملونة.",
+    comfort: "الراحة المميزة",
+    comfortDesc:
+      "يتم تصنيع المقاعد الأمامية وفقًا لشروط سيارات الرياضية بوسادة رأس واحدة مرتفعة، ودعم جانبي متقدم للوسائد، وجلد صناعي مثقب عالي الجودة.",
+    seats: "المقاعد الرياضية",
+    seatsDesc:
+      "تتميز المقاعد الأمامية الرياضية بالتدفئة والتهوية وتحتوي على مجموعة متنوعة من التعديلات الكهربائية لراحة السائق والراكب",
+    safety: "الديناميات والسلامة",
+    safetyDesc:
+      "تؤمن توليفة المحرك التوربو 1.6 الفعال وناقل الحركة CVT9 ديناميكية قيادة ممتازة وراحة. يتم إقران المحرك مع صندوق تروس موثوق ومتين، مجهز بتربوشارجر Honeywell وتكنولوجيا DVVT.",
+    performance: "أداء وصديق للبيئة",
+    performanceDesc:
+      "القوة القصوى لمحرك 1.6 توربو هي 147 حصانًا ، وأقصى عزم الدوران هو 210 نيوتن متر / 1750-4500 دورة في الدقيقة.",
+    ai: "أنظمة ADAS الذكية",
+    aiDesc:
+      "تشمل ADAS: التحكم في السرعة المتكيف، تحذير التصادم الأمامي، الكبح الطارئ الذاتي، تحذير الانحراف عن المسار، الحفاظ على المسار، وتبديل الشعاع المنخفض/العالي تلقائيًا.",
+    smart: "التكنولوجيا الذكية",
+    smartDesc:
+      "تحتوي الداخلية التقنية لـ أمودا الجديدة على العديد من الميزات المتقدمة: شاشتين ثنائيتي الدقة بحجم 10.25 بوصة لكل منهما، إضاءة داخلية جوية بألوان متعددة، شحن لاسلكي، صوت عالي الجودة والكثير غيرها.",
+    digital: "لوحة العدادات الرقمية",
+    digitalDesc:
+      "تم تقديم لوحة العدادات والمجمع المتعدد الوسائط على شكل شاشتي LCD مزدوجة بقطر 10.25 بوصة بدقة عالية ودعم لـ Android Auto و Apple CarPlay.",
+    charge: "شحن لاسلكي للهواتف ",
+    chargeDesc:
+      "وحدة الشحن اللاسلكية لهاتفين ذكيين لا تقوم فقط بشحنهما بكفاءة، ولكنها أيضًا تُعلِمك إذا لم يتم تركيب الجهاز في فتحة الشحن.",
+    remote: "تشغيل المحرك عن بُعد",
+    remoteDesc:
+      "تقنية البدء عن بُعد تسمح لك بتشغيل المحرك باستخدام مفتاح ذكي، وميزة الوصول المريح تسمح لك بفتح السيارة عند الاقتراب.",
+    sec: "أُمودا تشترك في نفس العقلية مع كل OMODER، وتلعب معًا، وهي أكثر العلامات التجارية المستقبلية في مجال سيارات الدفع الرباعي المتعددة الاستخدامات التي تستفيد من الشباب الأكثر.",
+    universe: "الكون الموحد",
+    universeDesc:
+      "أومودا هو مفتاح يمكنه الوصول إلى الأكوان الموازية، من خلاله يمكنك لقاء نفسك في مكان آخر واستكشاف إمكانيات لا حصر لها. أومودا يحترم كل نمط حياة ويلهم كل أومودر ليكون هو نفسه، ويظهر تألقًا لا حدود له من خلال أومودا. تعال من خلال المستقبل، أومودا،  اعبر من المستقبل،  اعبر من أجل المستقبل.",
+    community: "مجتمع من الأشخاص ذوي الفكر المشابه ",
+    communityDesc:
+      "يتميزون بالطاقة الرائعة ورغبتهم في الإبداع والتحسين المستمر وتوليد أفكار جديدة. أياً منها سيتحقق، سنعرف ذلك قريبًا جدًا - فالمستقبل أقرب بكثير مما يبدو.",
+    club: "O‑CLUB",
+    clubDesc:
+      "بدون خوف من المخاطرة، بفكرة استثنائية، بالإبداع والاستعداد لقبول الأشياء الجديدة... O-CLUB هو المجتمع الحصري لمعجبي أُمودرز سنبني منصة اجتماعية افتراضية عالمية، حيث يمكننا اللعب معًا مع المستخدمين العالميين، ومشاركة نفس القلب، والاستمرار في خلق ذكريات جيدة...",
+    sec2: "في مقدمة أومودا تواجد سفراؤنا، الذين إنجازاتهم وسجلاتهم تقترب من قدرات الإنسان، وتتجاوز الحدود.",
+    life: "O-LIFE",
+    lifeDesc:
+      "منظمة O-LIFE تدعو إلى مفهوم الأخضر  الكربون المنخفض، وحماية البيئة وتوفير الطاقة التي تأتي من خلال التكنولوجيا العالية، لتمثل المشاهد المستقبلية للحياة الذكية الايكولوجية.",
+    lab: "O-LAB",
+    labDesc:
+      "O-LAB هي منصة بحث موجهة نحو المستقبل ومختبر لتعاون الإبداع والإلهام. نحن نواصل التطوير والبحث وابتكار التقنيات الجديدة، ومشاركة وتجربة مع كل مستخدم؛ نبتكر ونمارس مع كل مستخدم، ونستكشف كل أرض غير مستكشفة معاً.",
+    fashion: "O-FASHION",
+    fashionDesc1:
+      "من الناس يحاولون الإمتداد في الفن والموسيقى والأزياء، وتدمير الحواجز الداخلية وتوسيع آفاقهم. هنا تذكرة غير محدودة إلى تجربة لا تُنسى تفتح أمام أصحاب أومودا وأصدقاء العلامة التجارية. انضم إلى نمط حياة الناس وكن جزءًا من الأحداث والأنشطة الفريدة التي أعدّها فريق العلامة التجارية خصيصًا لك.",
+    fashionDesc2:
+      "هو مفهوم جمالي مدفوع بالتكنولوجيا، يدمج بين التصميم البسيط والمستقبلية، يمثل اتجاه الجمالية للجيل المستقبلي. كما يعتمد على مجموعة أزياء قابلة للإرتداء  تكون ذكية بتصميم متقدم، يقدم بأسلوب جمالي يحاكي المستقبل.",
+    understand: "اعرف المزيد",
+    contactus: " تواصل معنا",
+  },
+};
+
+export default translation;
